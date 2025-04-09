bd854d18af10acd9b55168f0ce5de287856c405f
Maruthi Malapati <maruthi19955@gmail.com>
2025-04-09 17:12:29 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -1,23 +1,73 @@
 
+// const express = require('express');
+// const app = express();
+
+// const sequelize = require('./db');
+// const Ticket = require('./models/tickets');
+// const User =  require('./models/user')
+
+// const PORT = process.env.PORT || 3001;
+
+
+// const cors = require('cors');
+// app.use(cors());  // 🔥 allow frontend to access backend
+
+// app.use(express.json());
+
+// // in dev invi *
+// app.use(cors({
+//   origin: 'https://itsm-tool-frontend.vercel.app'
+// }));
+
+// app.post('/tickets', async (req, res) => {
+//   const ticket = await Ticket.create(req.body);
+//   res.json(ticket);
+// });
+
+// app.get('/tickets', async (req, res) => {
+//   const tickets = await Ticket.findAll();
+//   res.json(tickets);
+// });
+
+// app.post('/userdetails', async (req, res)=>{
+//   const users = await User.create(req.body);
+//   res.json(users);
+// });
+
+
+
+
+
+// app.listen(PORT, async () => {
+
+//   try {
+//     await sequelize.sync(); // ensures DB tables are created
+//     console.log(`Server running on http://localhost:${PORT}`);
+//   } catch (err) {
+//     console.error('DB error:', err);
+//   }
+// });
+
+
+
+
 const express = require('express');
 const app = express();
 
 const sequelize = require('./db');
 const Ticket = require('./models/tickets');
-const User =  require('./models/user')
+const User = require('./models/user');
 
 const PORT = process.env.PORT || 3001;
 
-
 const cors = require('cors');
-app.use(cors());  // 🔥 allow frontend to access backend
 
-app.use(express.json());
-
-// in dev invi *
+// ✅ Allow requests only from your frontend domain
 app.use(cors({
   origin: 'https://itsm-tool-frontend.vercel.app'
 }));
+
+app.use(express.json());
 
 app.post('/tickets', async (req, res) => {
   const ticket = await Ticket.create(req.body);
@@ -29,17 +79,12 @@ app.get('/tickets', async (req, res) => {
   res.json(tickets);
 });
 
-app.post('/userdetails', async (req, res)=>{
+app.post('/userdetails', async (req, res) => {
   const users = await User.create(req.body);
   res.json(users);
 });
 
-
-
-
-
 app.listen(PORT, async () => {
-
   try {
     await sequelize.sync(); // ensures DB tables are created
     console.log(`Server running on http://localhost:${PORT}`);
@@ -47,10 +92,6 @@ app.listen(PORT, async () => {
     console.error('DB error:', err);
   }
 });
-
-
-
-
 
 
 
