d5b3c0852f5f11ed6c362220db83a6349663c34e
Maruthi Malapati <maruthi19955@gmail.com>
2025-04-09 16:59:29 +0530

    added cors use

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(cors());  // 🔥 allow frontend to access backend
 
 app.use(express.json());
 
+// in dev invi *
+app.use(cors({
+  origin: 'https://itsm-tool-frontend.vercel.app'
+}));
+
 app.post('/tickets', async (req, res) => {
   const ticket = await Ticket.create(req.body);
   res.json(ticket);
@@ -52,65 +57,3 @@ app.listen(PORT, async () => {
 
 
 
-
-// const express = require("express");
-// const cors = require("cors");
-// require("dotenv").config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//     res.send("ITSM Ticketing API is running!");
-// });
-
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
-
-
-
-// /////
-// const users = [
-//     { id: 1, name: 'John Doe', email: 'john@example.com' },
-//     { id: 2, name: 'Jane Doe', email: 'jane@example.com' }
-//   ];
-  
-//   // Get all users
-//   app.get('/users', (req, res) => {
-//     res.json(users);
-//   });
-  
-//   // Get a single user by ID
-//   app.get('/users/:id', (req, res) => {
-//     const user = users.find(u => u.id === parseInt(req.params.id));
-//     user ? res.json(user) : res.status(404).json({ message: 'User not found' });
-//   });
-  
-//   // Add a new user
-//   app.post('/users', (req, res) => {
-//     const newUser = { id: users.length + 1, ...req.body };
-//     users.push(newUser);
-//     res.status(201).json(newUser);
-//   });
-  
-//   // Delete a user
-//   app.delete('/users/:id', (req, res) => {
-//     const index = users.findIndex(u => u.id === parseInt(req.params.id));
-//     if (index !== -1) {
-//       users.splice(index, 1);
-//       res.json({ message: 'User deleted' });
-//     } else {
-//       res.status(404).json({ message: 'User del not found' });
-//     }
-//   });
-
-  
-//   app.get('/users/id', (req, res) => {
-//     res.json({ message: "You're looking for users by ID. Use /users/{number}." });
-// });
-
-  
